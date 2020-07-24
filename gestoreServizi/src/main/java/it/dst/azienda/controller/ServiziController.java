b4e5cc1a568e773051cf70a165d0e73895b04ab4
Steve <Steve@DESKTOP-QFOD7V8.fritz.box>
2020-07-24 10:46:02 +0200

    a

--- gestoreServizi/src/main/java/it/dst/azienda/controller/ServiziController.java
+++ gestoreServizi/src/main/java/it/dst/azienda/controller/ServiziController.java
@@ -1,8 +1,60 @@
 package it.dst.azienda.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServiziController {
+	
+
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @RequestMapping(value = "public/login", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest, Device device, HttpServletResponse response) throws AuthenticationException, JsonProcessingException {
+
+        // Effettuo l autenticazione
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getUsername(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // Genero Token
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails, device);
+        response.setHeader(tokenHeader,token);
+        // Ritorno il token
+        return ResponseEntity.ok(new JwtAuthenticationResponse(userDetails.getUsername(),userDetails.getAuthorities()));
+    }
+
+    @RequestMapping(value = "protected/refresh-token", method = RequestMethod.GET)
+    public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request, HttpServletResponse response) {
+        String token = request.getHeader(tokenHeader);
+        UserDetails userDetails =
+                (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (jwtTokenUtil.canTokenBeRefreshed(token)) {
+            String refreshedToken = jwtTokenUtil.refreshToken(token);
+            response.setHeader(tokenHeader,refreshedToken);
+
+            return ResponseEntity.ok(new JwtAuthenticationResponse(userDetails.getUsername(),userDetails.getAuthorities()));
+        } else {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
 
 }
